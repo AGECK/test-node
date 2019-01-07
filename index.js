7785e3ed99e1066b4d39342c453ca13b8b9f4149
AGECK <46017447+AGECK@users.noreply.github.com>
2019-01-07 11:40:49 +0800

    Update index.js

--- index.js
+++ index.js
@@ -13,53 +13,78 @@ restService.use(
 
 restService.use(bodyParser.json());
 
-restService.post("/slack-test", function(req, res) {
+restService.post("/video", function(req, res) {
   return res.json({
-speech: "this text is spoken out loud if the platform supports voice interactions",
-displayText: "this text is displayed visually",
-messages: {
-  type: 1,
-  title: "card title",
-  subtitle: "card text",
-  imageUrl: "https://assistant.google.com/static/images/molecule/Molecule-Formation-stop.png"
-},
-data: {
-  google: {
-    expectUserResponse: true,
-    richResponse: {
-      items: [
-        {
-          simpleResponse: {
-            textToSpeech: "this is a simple response"
-          }
-        }
-      ]
-    }
-  },
-  facebook: {
-    text: "Hello, Facebook!"
-  },
-  slack: {
-    text: "This is a text response for Slack."
-  }
-},
-contextOut: [
-  {
-    name: "context name",
-    lifespan: 5,
-    parameters: {
-      param: "param value"
-    }
-  }
-],
-source: "example.com",
-followupEvent: {
-  name: "event name",
-  parameters: {
-    param: "param value"
-  }
-}
+    speech:
+      '<speak>  <audio src="https://www.youtube.com/watch?v=VX7SSnvpj-8">did not get your MP3 audio file</audio></speak>',
+    displayText:
+      '<speak>  <audio src="https://www.youtube.com/watch?v=VX7SSnvpj-8">did not get your MP3 audio file</audio></speak>',
+    source: "webhook-echo-sample"
+  });
+});
 
+restService.post("/slack-test", function(req, res) {
+  var slack_message = {
+    text: "Details of JIRA board for Browse and Commerce",
+    attachments: [
+      {
+        title: "JIRA Board",
+        title_link: "http://www.google.com",
+        color: "#36a64f",
+
+        fields: [
+          {
+            title: "Epic Count",
+            value: "50",
+            short: "false"
+          },
+          {
+            title: "Story Count",
+            value: "40",
+            short: "false"
+          }
+        ],
+
+        thumb_url:
+          "https://stiltsoft.com/blog/wp-content/uploads/2016/01/5.jira_.png"
+      },
+      {
+        title: "Story status count",
+        title_link: "http://www.google.com",
+        color: "#f49e42",
+
+        fields: [
+          {
+            title: "Not started",
+            value: "50",
+            short: "false"
+          },
+          {
+            title: "Development",
+            value: "40",
+            short: "false"
+          },
+          {
+            title: "Development",
+            value: "40",
+            short: "false"
+          },
+          {
+            title: "Development",
+            value: "40",
+            short: "false"
+          }
+        ]
+      }
+    ]
+  };
+  return res.json({
+    speech: "speech",
+    displayText: "speech",
+    source: "webhook-echo-sample",
+    data: {
+      slack: slack_message
+    }
   });
 });
 
