1160303c16fa11038852203264cf4c9fee4a956b
AGECK <46017447+AGECK@users.noreply.github.com>
2019-01-07 11:24:36 +0800

    Update index.js

--- index.js
+++ index.js
@@ -15,48 +15,48 @@ restService.use(bodyParser.json());
 
 restService.post("/slack-test", function(req, res) {
   return res.json({
-"speech": "this text is spoken out loud if the platform supports voice interactions",
-"displayText": "this text is displayed visually",
-"messages": {
-  "type": 1,
-  "title": "card title",
-  "subtitle": "card text",
-  "imageUrl": "https://assistant.google.com/static/images/molecule/Molecule-Formation-stop.png"
+speech: "this text is spoken out loud if the platform supports voice interactions",
+displayText: "this text is displayed visually",
+messages: {
+  type: 1,
+  title: "card title",
+  subtitle: "card text",
+  imageUrl: "https://assistant.google.com/static/images/molecule/Molecule-Formation-stop.png"
 },
-"data": {
-  "google": {
-    "expectUserResponse": true,
-    "richResponse": {
-      "items": [
+data: {
+  google: {
+    expectUserResponse: true,
+    richResponse: {
+      items: [
         {
-          "simpleResponse": {
-            "textToSpeech": "this is a simple response"
+          simpleResponse: {
+            textToSpeech: "this is a simple response"
           }
         }
       ]
     }
   },
-  "facebook": {
-    "text": "Hello, Facebook!"
+  facebook: {
+    text: "Hello, Facebook!"
   },
-  "slack": {
-    "text": "This is a text response for Slack."
+  slack: {
+    text: "This is a text response for Slack."
   }
 },
-"contextOut": [
+contextOut: [
   {
-    "name": "context name",
-    "lifespan": 5,
-    "parameters": {
-      "param": "param value"
+    name: "context name",
+    lifespan: 5,
+    parameters: {
+      param: "param value"
     }
   }
 ],
-"source": "example.com",
-"followupEvent": {
-  "name": "event name",
-  "parameters": {
-    "param": "param value"
+source: "example.com",
+followupEvent: {
+  name: "event name",
+  parameters: {
+    param: "param value"
   }
 }
 
