f44749838968d36cd1f35f449f0bac1f14810c32
AGECK <46017447+AGECK@users.noreply.github.com>
2019-01-25 12:16:32 +0800

    Update index.js

--- index.js
+++ index.js
@@ -21,54 +21,14 @@ restService.post("/V2test",function(req,res){
            	  fulfillmentText: "This is a text response",
            	  fulfillmentMessages:[
            	  {
-           	  	    card: {
-			        title: "card title",
-			        subtitle: "card text",
-			        imageUri: "https://assistant.google.com/static/images/molecule/Molecule-Formation-stop.png",
-			        buttons: [
-			          {
-			            text: "button text",
-			            postback: "https://assistant.google.com/"
-			          }
-			        ]
-			      }
+                   text :{
+                   	  text : [
+                   	     "baidu"
+                   	  ]
+                   }
            	   }
            	  ],
            	  source: "webhook-echo-sample",
-           	      payload: {
-				    google: {
-				      expectUserResponse: true,
-				      richResponse: {
-				        items: [
-				          {
-				            simpleResponse: {
-				              textToSpeech: "This is a Basic Card:"
-				            }
-				          },
-				          {
-				            basicCard: {
-				              title: "Card Title",
-				              image: {
-				                url: "https://raw.githubusercontent.com/AGECK/test-node/master/public/test-img.png",
-				                accessibilityText: "Google Logo",
-				                height: "200",
-				                width: "100"
-				              },
-				              buttons: [
-				                {
-				                  title: "点我百度",
-				                  openUrlAction: {
-				                    url: "https://www.baidu.com"
-				                  }
-				                }
-				              ],
-				              imageDisplayOptions: "DEFAULT"
-				            }
-				          }
-				        ]
-				      }
-				    }
-				  }
            });
         break;
     }
