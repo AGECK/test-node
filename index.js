41dceb39b6972eaad604d428673f16e3ea59757e
AGECK <46017447+AGECK@users.noreply.github.com>
2019-01-25 12:04:21 +0800

    Update index.js

--- index.js
+++ index.js
@@ -6,68 +6,75 @@ const bodyParser = require("body-parser");
 const restService = express();
 
 restService.use(
-  bodyParser.urlencoded({
-    extended: true
-  })
-);
+    bodyParser.urlencoded({
+    	extended: true
+    })
+	);
 
 restService.use(bodyParser.json());
 
-restService.post("/V2test", function(req, res) {
-  return res.json({
-    fulfillmentText: "This is a text response",
-    fulfillmentMessages: [
-      {
-        card: {
-        title: "card title",
-        subtitle: "card text",
-        imageUri: "https://assistant.google.com/static/images/molecule/Molecule-Formation-stop.png",
-        buttons: [
-          {
-            text: "button text",
-            postback: "https://assistant.google.com/"
-          }
-        ]
-      }
-      }
-    ],
-    source: "webhook-echo-sample",
-    payload: {
-    google: {
-      expectUserResponse: true,
-      richResponse: {
-        items: [
-          {
-            simpleResponse: {
-              textToSpeech: "This is a Basic Card:"
-            }
-          },
-          {
-            basicCard: {
-              title: "Baidu搜索",
-              image: {
-                url: "https://ss0.bdstatic.com/5aV1bjqh_Q23odCf/static/superman/img/logo_top_86d58ae1.png",
-                accessibilityText: "Baidu Logo",
-                height: 300,
-                width: 200
-              },
-              buttons: [
-                {
-                  title: "点我百度",
-                  openUrlAction: {
-                    url: "https://www.baidu.com"
-                  }
-                }
-              ],
-              imageDisplayOptions: "CROPPED"
-            }
-          }
-        ]
-      }
+restService.post("/V2test",function(req,res){
+    switch(res.body.result.parameters.AudioSample.toLowerCase())
+    {
+        case "baidu":
+           return res.json({
+           	  fulfillmentText: "This is a text response",
+           	  fulfillmentMessages:[
+           	  {
+           	  	    card: {
+			        title: "card title",
+			        subtitle: "card text",
+			        imageUri: "https://assistant.google.com/static/images/molecule/Molecule-Formation-stop.png",
+			        buttons: [
+			          {
+			            text: "button text",
+			            postback: "https://assistant.google.com/"
+			          }
+			        ]
+			      }
+           	   }
+           	  ],
+           	  source: "webhook-echo-sample",
+           	      payload: {
+				    google: {
+				      expectUserResponse: true,
+				      richResponse: {
+				        items: [
+				          {
+				            simpleResponse: {
+				              textToSpeech: "This is a Basic Card:"
+				            }
+				          },
+				          {
+				            basicCard: {
+				              title: "Card Title",
+				              image: {
+				                url: "https://raw.githubusercontent.com/AGECK/test-node/master/public/test-img.png",
+				                accessibilityText: "Google Logo",
+				                height: "200",
+				                width: "100"
+				              },
+				              buttons: [
+				                {
+				                  title: "点我百度",
+				                  openUrlAction: {
+				                    url: "https://www.baidu.com"
+				                  }
+				                }
+				              ],
+				              imageDisplayOptions: "DEFAULT"
+				            }
+				          }
+				        ]
+				      }
+				    }
+				  }
+           });
+        break;
     }
-  }
-  });
 });
-restService.listen(process.env.PORT || 8000, function() {
-  console.log("Server up and listening");
-});
+
+
+restService.listen(process.env.POST || 8000,function(){
+	console.log("Server up and listening");
+}); 
