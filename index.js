fd1a27eb99ba3298034d467caa7dba73ebbe5b51
AGECK <46017447+AGECK@users.noreply.github.com>
2019-01-28 10:56:10 +0800

    Update index.js

--- index.js
+++ index.js
@@ -18,14 +18,63 @@ restService.post("/V2test",function(req,res){
            	  fulfillmentText: "This is a text response",
            	  fulfillmentMessages:[
            	  {
-                      text : {
-		          text : [
-			      "baidu"
-			  ]
-		      }
+                   text :{
+                   	  text : [
+                   	     "baidu"
+                   	  ]
+                   }
            	   }
            	  ],
-           	  source: "webhook-echo-sample"
+           	  source: "webhook-echo-sample",
+           	    payload: {
+			    google: {
+			      expectUserResponse: true,
+			      richResponse: {
+			        items: [
+			          {
+			            simpleResponse: {
+			              textToSpeech: "This is a Basic Card:"
+			            }
+			          },
+			          {
+/*			            basicCard: {
+			              title: "Card Title",
+			              image: {
+			                url: "https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png",
+			                accessibilityText: "Google Logo"
+			              },
+			              buttons: [
+			                {
+			                  title: "Button Title",
+			                  openUrlAction: {
+			                    url: "https://www.google.com"
+			                  }
+			                }
+			              ],
+			              imageDisplayOptions: "WHITE"
+			            }*/
+
+                        listSelect : {
+                        	title : "This is test listSelect",
+                        	items : [{
+                        		info : {
+                                     key : "baidu"
+                        		}
+                        		title : "These are some of the options",
+                        		description : "These are some of the options",
+                        		image : {
+                        			imageUri : "https://stiltsoft.com/blog/wp-content/uploads/2016/01/5.jira_.png",
+                        			accessibilityText : "Google Logo"
+                        		}
+                        	}]
+                        }
+
+
+			          }
+			        ]
+			      }
+			    }
+ 			 }
            });
 });
 
