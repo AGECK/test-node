45aee0c6637652abafe881712db224464b1e871b
AGECK <46017447+AGECK@users.noreply.github.com>
2019-01-28 16:15:41 +0800

    Update index.js

--- index.js
+++ index.js
@@ -6,77 +6,35 @@ const bodyParser = require("body-parser");
 const restService = express();
 
 restService.use(
-    bodyParser.urlencoded({
-    	extended: true
-    })
-	);
-
-restService.use(bodyParser.json());
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
 
 restService.post("/V2test",function(req,res){
+      var speech = req.body.result.parameters.AudioSample.toLowerCase();
+      if(speech == "baidu")
+      {
            return res.json({
-           	  fulfillmentText: "This is a text response",
-           	  fulfillmentMessages:[
-           	  {
+              fulfillmentText: "This is a text response",
+              fulfillmentMessages:[
+              {
                    text :{
-                   	  text : [
-                   	     "baidu"
-                   	  ]
+                      text : [
+                         "baidu"
+                      ]
                    }
-           	   }
-           	  ],
-           	  source: "webhook-echo-sample",
-			 payload: {
-			    google: {
-			      expectUserResponse: true,
-			      richResponse: {
-			        items: [
-			          {
-			            simpleResponse: {
-			              textToSpeech: "Choose a item"
-			            }
-			          }
-			        ]
-			      },
-			      systemIntent: {
-			        intent: "actions.intent.OPTION",
-			        data: {
-			          "@type": "type.googleapis.com/google.actions.v2.OptionValueSpec",
-			          listSelect: {
-			            title: "Hello",
-			            items: [
-			              {
-			                optionInfo: {
-			                  key: "first title key"
-			                },
-			                description: "first description",
-			                image: {
-			                  url: "https://developers.google.com/actions/images/badges/XPM_BADGING_GoogleAssistant_VER.png",
-			                  accessibilityText: "first alt"
-			                },
-			                title: "first title"
-			              },
-			              {
-			                optionInfo: {
-			                  key: "second"
-			                },
-			                description: "second description",
-			                image: {
-			                  url: "https://lh3.googleusercontent.com/Nu3a6F80WfixUqf_ec_vgXy_c0-0r4VLJRXjVFF_X_CIilEu8B9fT35qyTEj_PEsKw",
-			                  accessibilityText: "second alt"
-			                },
-			                title: "second title"
-			              }
-			            ]
-			          }
-			        }
-			      }
-			    }
-			  }
+               }
+              ],
+              source: "webhook-echo-sample"
            });
+      }
 });
 
 
+restService.use(bodyParser.json());
+
+
 restService.listen(process.env.PORT || 8000,function(){
-	console.log("Server up and listening");
-}); 
+     console.log("Server up and listening");
+});
