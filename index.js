a1aeecd32d4ef53ce5891a3d460e792ee2a925a4
AGECK <46017447+AGECK@users.noreply.github.com>
2019-02-16 17:28:51 +0800

    Update index.js

--- index.js
+++ index.js
@@ -61,12 +61,12 @@ restService.post("/V2test",function(req,res){
 				      }
 				    }
 				  },
-		"followupEventInput": {
-		    "name": "actions_intent_OPTION",
-		    "parameters": {
-			"param1": "this is it"
+		followupEventInput: {
+		    name: "actions_intent_OPTION",
+		    parameters: {
+			param1: "this is it"
 		    },
-		  "languageCode":"en-US"
+		  languageCode:"en-US"
 		}
      	});
 });
