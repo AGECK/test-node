3edd4c625ebfdc68bba09e90425054cddad89f79
AGECK <46017447+AGECK@users.noreply.github.com>
2019-02-16 10:16:53 +0800

    Update index.js

--- index.js
+++ index.js
@@ -13,134 +13,25 @@ restService.use(
 
 restService.use(bodyParser.json());
 
+
 restService.post("/V2test",function(req,res){
-          var speech = "";
-        
-       switch(req.body.queryResult.parameters.echoText.toLowerCase()){
-       	  case "baidu":
-           return res.json({
-              fulfillmentText: "This is a text response",
-              fulfillmentMessages:[
-              {
-                   text :{
-                      text : [
-                          "This is Baidu"
-                      ]
-                   }
-               }
-              ],
-              source: "webhook-echo-sample",
-                  payload: {
-			google: {
-				expectUserResponse: true,
-				richResponse: {
-				items: [
-				{
-				  simpleResponse: {
-				      textToSpeech: "This is a Basic Card:"
-				     }
-				       },
-				     {
-				       basicCard: {
-				         title: "百度",
-				         image: {
-				         url: "https://ss0.bdstatic.com/5aV1bjqh_Q23odCf/static/superman/img/logo_top_86d58ae1.png",
-				         accessibilityText: "Baidu Logo",
-				         height: "200",
-				         width: "100"
-				              },
-				         buttons: [
-				                {
-				            title: "点我百度",
-				            openUrlAction: {
-				            url: "https://www.baidu.com"
-				                 }
-				                }
-				              ],
-				            imageDisplayOptions: "DEFAULT"
-				            }
-				          }
-				        ]
-				      }
-				    }
-				  }
-           });
-           break;
-            case "related products":
-                      return res.json({
-           	  fulfillmentText: "This is a text response",
-           	  fulfillmentMessages:[
-           	  {
-                   text :{
-                   	  text : [
-                   	     "baidu"
-                   	  ]
-                   }
-           	   }
-           	  ],
-           	  source: "webhook-echo-sample",
-			 payload: {
-			    google: {
-			      expectUserResponse: true,
-			      richResponse: {
-			        items: [
-			          {
-			            simpleResponse: {
-			              textToSpeech: "Choose a item"
-			            }
-			          }
-			        ]
-			      },
-			      systemIntent: {
-			        intent: "actions.intent.OPTION",
-			        data: {
-			          "@type": "type.googleapis.com/google.actions.v2.OptionValueSpec",
-			          listSelect: {
-			            title: "Hello",
-			            items: [
-			              {
-			                optionInfo: {
-			                  key: "first title key"
-			                },
-			                description: "first description",
-			                image: {
-			                  url: "https://developers.google.com/actions/images/badges/XPM_BADGING_GoogleAssistant_VER.png",
-			                  accessibilityText: "first alt"
-			                },
-			                title: "first title"
-			              },
-			              {
-			                optionInfo: {
-			                  key: "second"
-			                },
-			                description: "second description",
-			                image: {
-			                  url: "https://lh3.googleusercontent.com/Nu3a6F80WfixUqf_ec_vgXy_c0-0r4VLJRXjVFF_X_CIilEu8B9fT35qyTEj_PEsKw",
-			                  accessibilityText: "second alt"
-			                },
-			                title: "second title"
-			              }
-			            ]
-			          }
-			        },
-				followupEventInput: {
-				name: "actions_intent_OPTION",
-				parameters: {
-				echoText : "baidu"
-					 },
-			        languageCode: "en-US"
-				}
-			      }
-			    }
-			  }
-			 
-           });
-          break;
-      }
+ 
+      app.intent('action.intent.OPTIONS',(conv,params,option) => {
+      	  if(!option)
+      	  {
+                conv.ask('You did not select any item from the list or carousel');
+      	  }
+      	  else if(option === 'baidu')
+      	  {
+              conv.ask('You onClick the Baidu');
+      	  }
+      	  else
+      	  {
+      	  	conv.ask('You selected an unknown item from the list,or carousel');
+      	  }
+      });
+
 });
-
-
-
 
 
 restService.listen(process.env.PORT || 8000,function(){
