8a92a7e6b462741eb105d7455981c598cb044094
AGECK <46017447+AGECK@users.noreply.github.com>
2019-02-19 15:01:48 +0800

    Update index.js

--- index.js
+++ index.js
@@ -15,59 +15,21 @@ restService.use(bodyParser.json());
 
 
 restService.post("/V2test",function(req,res){
-     	return res.json({
+	    var Event = "";
+	    Event=req.body.inputs.arguments.textValue;
+     	return res.json({           
               fulfillmentText: "This is a text response",
               fulfillmentMessages:[
               {
                    text :{
                       text : [
-                          "This is Baidu"
+                          Event
                       ]
                    }
                }
               ],
-              source: "webhook-echo-sample",
-                  payload: {
-				    google: {
-				      expectUserResponse: true,
-				      richResponse: {
-				        items: [
-				          {
-				            simpleResponse: {
-				              textToSpeech: "This is a Basic Card:"
-				            }
-				          },
-				          {
-				            basicCard: {
-				              title: "百度",
-				              image: {
-				                url: "https://ss0.bdstatic.com/5aV1bjqh_Q23odCf/static/superman/img/logo_top_86d58ae1.png",
-				                accessibilityText: "Baidu Logo",
-				                height: "200",
-				                width: "100"
-				              },
-				              buttons: [
-				                {
-				                  title: "点我百度",
-				                  openUrlAction: {
-				                    url: "https://www.baidu.com"
-				                  }
-				                }
-				              ],
-				              imageDisplayOptions: "DEFAULT"
-				            }
-				          }
-				        ]
-				      }
-				    }
-				  },
-		followupEventInput: {
-		    name: "actions_intent_OPTION",
-		    parameters: {
-			param1: "this is it"
-		    },
-		  languageCode:"en-US"
-		}
+              source: "webhook-echo-sample"
+
      	});
 });
 
