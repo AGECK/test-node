b66a84f2683f598f6567fdc4f9f49c8fd09ae647
AGECK <46017447+AGECK@users.noreply.github.com>
2019-02-26 14:14:25 +0800

    Update index.js

--- index.js
+++ index.js
@@ -14,31 +14,57 @@ restService.use(
 restService.use(bodyParser.json());
 
 restService.post("/V2test",function(){
-     	    return res.json({
+return res.json({
     fulfillmentText: "This is a text response",
     fulfillmentMessages: [
       {
-           text:{
-           	  text:[
-                 "This is a song called music"
-           	  ]
-           }
+        card: {
+        title: "card title",
+        subtitle: "card text",
+        imageUri: "https://assistant.google.com/static/images/molecule/Molecule-Formation-stop.png",
+        buttons: [
+          {
+            text: "button text",
+            postback: "https://assistant.google.com/"
+          }
+        ]
+      }
       }
     ],
     source: "webhook-echo-sample",
     payload: {
     google: {
-    expectUserResponse: true,
-    richResponse: {
-      items: [
-        {
-          simpleResponse: {
-            textToSpeech: "<speak><audio src='https://raw.githubusercontent.com/AGECK/test-node/master/public/Cancan.mp3'>your wave file</audio></speak>"
+      expectUserResponse: true,
+      richResponse: {
+        items: [
+          {
+            simpleResponse: {
+              textToSpeech: "This is a Basic Card:"
+            }
+          },
+          {
+            basicCard: {
+              title: "Card Title",
+              image: {
+                url: "https://raw.githubusercontent.com/AGECK/test-node/master/public/test-img.png",
+                accessibilityText: "Google Logo",
+                height: "200",
+                width: "100"
+              },
+              buttons: [
+                {
+                  title: "点我百度",
+                  openUrlAction: {
+                    url: "https://www.baidu.com"
+                  }
+                }
+              ],
+              imageDisplayOptions: "DEFAULT"
+            }
           }
-        }
-      ]
+        ]
+      }
     }
-  }
   }
   });
 });
