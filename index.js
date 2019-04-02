95b169612a11c679290bf1950d3204c5226fd106
AGECK <46017447+AGECK@users.noreply.github.com>
2019-04-02 11:12:03 +0800

    Update index.js

--- index.js
+++ index.js
@@ -13,53 +13,33 @@ restService.use(
 
 restService.use(bodyParser.json());
 
-restService.post("/V2test",function(){
-return res.json({
-              fulfillmentText: "This is a text response",
-              fulfillmentMessages:[
-              {
-                   text :{
-                      text : [
-                          "This is Baidu"
-                      ]
-                   }
-               }
-              ],
-              source: "webhook-echo-sample",
-                  payload: {
-				    google: {
-				      expectUserResponse: true,
-				      richResponse: {
-				        items: [
-				          {
-				            simpleResponse: {
-				              textToSpeech: "This is a Basic Card:"
-				            }
-				          },
-				          {
-				            basicCard: {
-				              title: "百度",
-				              image: {
-				                url: "https://ss0.bdstatic.com/5aV1bjqh_Q23odCf/static/superman/img/logo_top_86d58ae1.png",
-				                accessibilityText: "Baidu Logo",
-				                height: "200",
-				                width: "100"
-				              },
-				              buttons: [
-				                {
-				                  title: "点我百度",
-				                  openUrlAction: {
-				                    url: "https://www.baidu.com"
-				                  }
-				                }
-				              ],
-				              imageDisplayOptions: "DEFAULT"
-				            }
-				          }
-				        ]
-				      }
-				    }
-				  }
+restService.post("/V2test",function(req, res){
+	    return res.json({
+    fulfillmentText: "This is a text response",
+    fulfillmentMessages: [
+      {
+           text:{
+           	  text:[
+                 "This is a song called music"
+           	  ]
+           }
+      }
+    ],
+    source: "webhook-echo-sample",
+    payload: {
+    google: {
+    expectUserResponse: true,
+    richResponse: {
+      items: [
+        {
+          simpleResponse: {
+            textToSpeech: "<speak><audio src='https://raw.githubusercontent.com/AGECK/test-node/master/public/Cancan.mp3'>your wave file</audio></speak>"
+          }
+        }
+      ]
+    }
+  }
+  }
   });
 });
 
