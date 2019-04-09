bc8db6ef97ac6dd13796a8427737167738aef22c
AGECK <46017447+AGECK@users.noreply.github.com>
2019-04-09 11:33:45 +0800

    Update index.js

--- index.js
+++ index.js
@@ -14,40 +14,92 @@ restService.use(
 restService.use(bodyParser.json());
 
 restService.post("/V2test",function(req, res){
-	    return res.json({
-    fulfillmentText: "This is a text response",
-    fulfillmentMessages: [
+
+var userspeech="";
+switch (req.body.result.parameters.AudioSample.toLowerCase()) {
+
+         case "stop":
+         return res.json({
+                          fulfillmentText: "This is a text response",
+              fulfillmentMessages:[
+              {
+                   text :{
+                      text : [
+                          "This is Baidu"
+                      ]
+                   }
+               }
+              ],
+              source: "webhook-echo-sample",
+                  payload: {
+				    google: {
+				      expectUserResponse: true,
+				      richResponse: {
+				        items: [
+				          {
+				            simpleResponse: {
+				              textToSpeech: "Stop repeat the voice"
+				            }
+				          }
+				        ]
+				      }
+				    }
+				  },
+    outputContexts: [
       {
-           text:{
-           	  text:[
-                 "This is a song called music"
-           	  ]
-           }
+        name: "projects/jituopi-5f232/agent/sessions/ad236f60-2b12-2a60-ccff-b35ca09609f4/contexts/repeatthevoice-followup",
+        lifespanCount: 0
       }
-    ],
-    source: "webhook-echo-sample",
-    payload: {
-    google: {
-    expectUserResponse: true,
-    richResponse: {
-      items: [
-        {
-          simpleResponse: {
-            textToSpeech: "<speak><audio src='https://raw.githubusercontent.com/AGECK/test-node/master/public/Cancan.mp3'>your wave file</audio></speak>"
-          }
-        }
-      ]
-    }
-  }
-  },
-"outputContexts": [
-      {
-        "name": "projects/jituopi-5f232/agent/sessions/ad236f60-2b12-2a60-ccff-b35ca09609f4/contexts/repeatthevoice-followup",
-        "lifespanCount": 1
-      }
-    ]		
-  });
-});
+    ]
+
+         });
+         break;
+         case "play music":
+
+		    return res.json({
+	    fulfillmentText: "This is a text response",
+	    fulfillmentMessages: [
+	      {
+	           text:{
+	           	  text:[
+	                 "This is a song called music"
+	           	  ]
+	           }
+	      }
+	    ],
+	    source: "webhook-echo-sample",
+	    payload: {
+	    google: {
+	    expectUserResponse: true,
+	    richResponse: {
+	      items: [
+	        {
+	          simpleResponse: {
+	            textToSpeech: "<speak><audio src='https://raw.githubusercontent.com/AGECK/test-node/master/public/Cancan.mp3'>your wave file</audio></speak>"
+	          }
+	        }
+	      ]
+	    }
+	  }
+	  },
+	  "outputContexts": [
+	      {
+	        "name": "projects/jituopi-5f232/agent/sessions/ad236f60-2b12-2a60-ccff-b35ca09609f4/contexts/repeatthevoice-followup",
+	        "lifespanCount": 1
+	      }
+	    ]		
+	  });
+
+         break;
+
+
+}
+	  });
+
+
+
+
+
 
 
 
