3d67a7b0bc6f20e5ee3badc53d3dfcf11afdcec3
AGECK <46017447+AGECK@users.noreply.github.com>
2019-04-11 10:57:09 +0800

    Update index.js

--- index.js
+++ index.js
@@ -18,9 +18,46 @@ restService.post("/V2test",function(req, res){
 var userspeech="";
 switch (req.body.queryResult.queryText.toLowerCase()) {
 
+	case "Stop to follow":
+         return res.json({
+              fulfillmentText: "Stop repeat the voice",
+              fulfillmentMessages:[
+              {
+                   text :{
+                      text : [
+                          "Stop repeat the voice"
+                      ]
+                   }
+               }
+              ],
+              source: "webhook-echo-sample",
+                  payload: {
+				    google: {
+				      expectUserResponse: true,
+				      richResponse: {
+				        items: [
+				          {
+				            simpleResponse: {
+				              textToSpeech: "Stop repeat the voice"
+				            }
+				          }
+				        ]
+				      }
+				    }
+				  },
+	    outputContexts: [
+	      {
+		name: "projects/jituopi-5f232/agent/sessions/ad236f60-2b12-2a60-ccff-b35ca09609f4/contexts/repeatthevoice-followup",
+		lifespanCount: 0
+	      }
+	    ]
+
+         });
+	 break;
+	 
          case "stop restudy":
          return res.json({
-                          fulfillmentText: "Stop repeat the voice",
+              fulfillmentText: "Stop repeat the voice",
               fulfillmentMessages:[
               {
                    text :{
